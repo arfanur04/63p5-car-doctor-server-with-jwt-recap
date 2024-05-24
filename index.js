4ed87dcbbee6f1e84d45f80664ef9845a2a56cfb
arfanur04 <arfanur04@gmail.com>
2024-05-24 11:35:29 +0600

    clone repo, initial setup

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const port = process.env.PORT || 5000;
 app.use(
 	cors({
 		origin: [
-			// 'http://localhost:5173',
-			"https://cars-doctor-6c129.web.app",
-			"https://cars-doctor-6c129.firebaseapp.com",
+			"http://localhost:5173",
+			"https://m58-car-doctor.web.app",
+			"https://m58-car-doctor.firebaseapp.com",
 		],
 		credentials: true,
 	})
@@ -21,9 +21,7 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-console.log(process.env.DB_PASS);
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zxotz8q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -36,24 +34,34 @@ const client = new MongoClient(uri, {
 
 // middlewares
 const logger = (req, res, next) => {
-	console.log("log: info", req.method, req.url);
-	next();
+	try {
+		console.log("log: info", req.method, req.url);
+		next();
+	} catch (error) {
+		console.error("error: ", error);
+		res.status(500).send({ message: "Internal Server Error" });
+	}
 };
 
 const verifyToken = (req, res, next) => {
-	const token = req?.cookies?.token;
-	// console.log('token in the middleware', token);
-	// no token available
-	if (!token) {
-		return res.status(401).send({ message: "unauthorized access" });
-	}
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-		if (err) {
+	try {
+		const token = req?.cookies?.token;
+		// console.log('token in the middleware', token);
+		// no token available
+		if (!token) {
 			return res.status(401).send({ message: "unauthorized access" });
 		}
-		req.user = decoded;
-		next();
-	});
+		jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+			if (err) {
+				return res.status(401).send({ message: "unauthorized access" });
+			}
+			req.user = decoded;
+			next();
+		});
+	} catch (error) {
+		console.error("error: ", error);
+		res.status(500).send({ message: "Internal Server Error" });
+	}
 };
 
 async function run() {
@@ -61,93 +69,131 @@ async function run() {
 		// Connect the client to the server	(optional starting in v4.7)
 		// await client.connect();
 
-		const serviceCollection = client.db("carDoctor").collection("services");
-		const bookingCollection = client.db("carDoctor").collection("bookings");
+		const serviceCollection = client.db("m58carDoctor").collection("services");
+		const bookingCollection = client.db("m58carDoctor").collection("bookings");
 
 		// auth related api
 		app.post("/jwt", logger, async (req, res) => {
-			const user = req.body;
-			console.log("user for token", user);
-			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-				expiresIn: "1h",
-			});
+			try {
+				const user = req.body;
+				console.log("user for token", user);
+				const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+					expiresIn: "1h",
+				});
 
-			res
-				.cookie("token", token, {
-					httpOnly: true,
-					secure: true,
-					sameSite: "none",
-				})
-				.send({ success: true });
+				res
+					.cookie("token", token, {
+						httpOnly: true,
+						secure: true,
+						sameSite: "none",
+					})
+					.send({ success: true });
+			} catch (error) {
+				console.error("error: ", error);
+			}
 		});
 
 		app.post("/logout", async (req, res) => {
-			const user = req.body;
-			console.log("logging out", user);
-			res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+			try {
+				const user = req.body;
+				console.log("logging out", user);
+				res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+			} catch (error) {
+				res.status(500).send({ message: "Internal Server Error" });
+			}
 		});
 
 		// services related api
 		app.get("/services", async (req, res) => {
-			const cursor = serviceCollection.find();
-			const result = await cursor.toArray();
-			res.send(result);
+			try {
+				const cursor = serviceCollection.find();
+				const result = await cursor.toArray();
+				res.send(result);
+			} catch (error) {
+				console.error("error: ", error);
+				res.status(500).send({ message: "Internal Server Error" });
+			}
 		});
 
 		app.get("/services/:id", async (req, res) => {
-			const id = req.params.id;
-			const query = { _id: new ObjectId(id) };
+			try {
+				const id = req.params.id;
+				const query = { _id: new ObjectId(id) };
 
-			const options = {
-				// Include only the `title` and `imdb` fields in the returned document
-				projection: { title: 1, price: 1, service_id: 1, img: 1 },
-			};
+				const options = {
+					// Include only the `title` and `imdb` fields in the returned document
+					projection: { title: 1, price: 1, service_id: 1, img: 1 },
+				};
 
-			const result = await serviceCollection.findOne(query, options);
-			res.send(result);
+				const result = await serviceCollection.findOne(query, options);
+				res.send(result);
+			} catch (error) {
+				console.error("error: ", error);
+				res.status(500).send({ message: "Internal Server Error" });
+			}
 		});
 
 		// bookings
 		app.get("/bookings", logger, verifyToken, async (req, res) => {
-			console.log(req.query.email);
-			console.log("token owner info", req.user);
-			if (req.user.email !== req.query.email) {
-				return res.status(403).send({ message: "forbidden access" });
+			try {
+				console.log(req.query.email);
+				console.log("token owner info", req.user);
+				if (req.user.email !== req.query.email) {
+					return res.status(403).send({ message: "forbidden access" });
+				}
+				let query = {};
+				if (req.query?.email) {
+					query = { email: req.query.email };
+				}
+				const result = await bookingCollection.find(query).toArray();
+				res.send(result);
+			} catch (error) {
+				console.error("error: ", error);
+				res.status(500).send({ message: "Internal Server Error" });
 			}
-			let query = {};
-			if (req.query?.email) {
-				query = { email: req.query.email };
-			}
-			const result = await bookingCollection.find(query).toArray();
-			res.send(result);
 		});
 
 		app.post("/bookings", async (req, res) => {
-			const booking = req.body;
-			console.log(booking);
-			const result = await bookingCollection.insertOne(booking);
-			res.send(result);
+			try {
+				const booking = req.body;
+				console.log(booking);
+				const result = await bookingCollection.insertOne(booking);
+				res.send(result);
+			} catch (error) {
+				console.error("error: ", error);
+				res.status(500).send({ message: "Internal Server Error" });
+			}
 		});
 
 		app.patch("/bookings/:id", async (req, res) => {
-			const id = req.params.id;
-			const filter = { _id: new ObjectId(id) };
-			const updatedBooking = req.body;
-			console.log(updatedBooking);
-			const updateDoc = {
-				$set: {
-					status: updatedBooking.status,
-				},
-			};
-			const result = await bookingCollection.updateOne(filter, updateDoc);
-			res.send(result);
+			try {
+				const id = req.params.id;
+				const filter = { _id: new ObjectId(id) };
+				const updatedBooking = req.body;
+				console.log(updatedBooking);
+				const updateDoc = {
+					$set: {
+						status: updatedBooking.status,
+					},
+				};
+				const result = await bookingCollection.updateOne(filter, updateDoc);
+				res.send(result);
+			} catch (error) {
+				console.error("error: ", error);
+				res.status(500).send({ message: "Internal Server Error" });
+			}
 		});
 
 		app.delete("/bookings/:id", async (req, res) => {
-			const id = req.params.id;
-			const query = { _id: new ObjectId(id) };
-			const result = await bookingCollection.deleteOne(query);
-			res.send(result);
+			try {
+				const id = req.params.id;
+				const query = { _id: new ObjectId(id) };
+				const result = await bookingCollection.deleteOne(query);
+				res.send(result);
+			} catch (error) {
+				console.error("error: ", error);
+				res.status(500).send({ message: "Internal Server Error" });
+			}
 		});
 
 		// Send a ping to confirm a successful connection
---
+++ whenCloneRepo.js
@@ -0,0 +1,8 @@
+/* 
+   check this, if needed then update
+   1. change git remote set-url origin
+   2. cors origin
+   3. .env
+   4. const uri = 
+   5. mongodb db name, collection name
+ */
